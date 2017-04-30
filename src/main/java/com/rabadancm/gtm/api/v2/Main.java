28c4a2de5f729944693562487957e4ab234c0ef0
rabadancm <rabadancm@gmail.com>
2017-05-01 01:08:44 +0200

    Push test

--- src/main/java/com/rabadancm/gtm/api/v2/Main.java
+++ src/main/java/com/rabadancm/gtm/api/v2/Main.java
@@ -48,7 +48,7 @@ public class Main {
         System.out.println("Container: "+ cvh.getContainerId());
         System.out.println("Tags: "+ cvh.getNumTags());
         //accounts/900965899/containers/6004040
-        //6004040
+        ///6004040
         
                 
         // Containers
